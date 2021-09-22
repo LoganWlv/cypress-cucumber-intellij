9fc19ca0cc148f5f9c7268cc3e348552843d3660
GitHub Action <action@github.com>
2021-09-22 11:35:23 +0000

    Template cleanup

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,1 @@
-rootProject.name = "IntelliJ Platform Plugin Template"
+rootProject.name = "cypress-cucumber-intellij"
---
+++ src/main/kotlin/com/github/loganwlv/cypresscucumberintellij/services/MyApplicationService.kt
@@ -0,0 +1,10 @@
+package com.github.loganwlv.cypresscucumberintellij.services
+
+import com.github.loganwlv.cypresscucumberintellij.MyBundle
+
+class MyApplicationService {
+
+    init {
+        println(MyBundle.message("applicationService"))
+    }
+}
